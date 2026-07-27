7c239404888a1c008e888de14c5655383bcdae0b
Shubham Gahukar <sonaligahukar07@gmail.com>
2026-07-28 00:43:25 +0530

    feat(crud): implement create and read task operations

---
+++ backend/app/crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    new_task = models.Task(
+        title=task.title,
+        description=task.description,
+        priority=task.priority,
+        category=task.category,
+        due_date=task.due_date,
+        due_time=task.due_time,
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    return new_task
+
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
+    
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def update_task(db: Session, task_id: int, updated_task: schemas.TaskUpdate):
+    task = get_task(db, task_id)
+
+    if task is None:
+        return None
+
+    task.title = updated_task.title
+    task.description = updated_task.description
+    task.priority = updated_task.priority
+    task.category = updated_task.category
+    task.due_date = updated_task.due_date
+    task.due_time = updated_task.due_time
+    task.completed = updated_task.completed
+
+    db.commit()
+    db.refresh(task)
+
+    return task
