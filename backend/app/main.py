f9b717e1b8ecf0a21a55f712cda8e4fbc39a82cd
Shubham Gahukar <sonaligahukar07@gmail.com>
2026-08-09 17:54:35 +0530

    feat(frontend): connect task UI to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.database import engine
 from app import models
 from app.routes.task import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,6 +11,17 @@ app = FastAPI(
     title="Nova API",
     version="0.1.0",
     description="AI Productivity Assistant Backend"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
